dfef52f878664fe026ad9145717da7ec6daebf6c
alexguirre <alexguirre.aa@gmail.com>
2025-10-12 11:55:45 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sollumz I/O
+
+Library for importing and exporting RAGE asset files.
+
+## Installation
+
+```sh
+pip install szio
+```
---
+++ tests/test_shader_manager.py
@@ -0,0 +1,25 @@
+import pytest
+from szio.gta5 import ShaderManager
+
+
+@pytest.mark.parametrize("filename, expected", (
+    ("default.sps", "default.sps"),
+    ("hash_18ad1594", "default.sps"),
+    ("hash_18AD1594", "default.sps"),
+    ("terrain_cb_4lyr.sps", "terrain_cb_4lyr.sps"),
+    ("hash_C8D15397", "terrain_cb_4lyr.sps"),
+))
+def test_find_shader(filename: str, expected: str):
+    shader = ShaderManager.find_shader(filename)
+    assert shader is not None
+    assert shader.filename == expected
+
+
+@pytest.mark.parametrize("filename", (
+    "unknown.sps",
+    "hash_1234ABCD",
+    "",
+))
+def test_find_shader_unknown_returns_none(filename: str):
+    shader = ShaderManager.find_shader(filename)
+    assert shader is None
