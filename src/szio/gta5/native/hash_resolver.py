5b624752e793c1776290556f6701d32fdccb3ba1
alexguirre <alexguirre.aa@gmail.com>
2026-05-17 17:18:54 +0200

    fix(native): reference leak of hash resolver
    The hash resolver instance was kept around through the global
    `_get_resolver._instance`, causing the following warning from nanobind
    when the process exits:
    ```
    nanobind: leaked 1 instances!
        - leaked instance 00007FFA437F4970 of type "pymateria._pymateria.HashResolver"
    ```
    Harmless but an annoying warning.

--- src/szio/gta5/native/hash_resolver.py
+++ src/szio/gta5/native/hash_resolver.py
@@ -2,9 +2,6 @@ import pymateria.gta5 as pm
 
 
 class NativeHashResolver:
-    def __init__(self):
-        self._instance = pm.HashResolver.instance
-
     def load_cache(self, path):
         pass
 
@@ -12,7 +9,7 @@ class NativeHashResolver:
         pass
 
     def load_nametable(self, nt: str):
-        self._instance.load_nametable(nt)
+        pm.HashResolver.instance.load_nametable(nt)
 
     def resolve_string(self, hash_value: int) -> str | None:
-        return self._instance.resolve_string(hash_value)
+        return pm.HashResolver.instance.resolve_string(hash_value)
